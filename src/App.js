9eb47ad05e8568abcbbd3f793343636952a0938a
Danielle Koski <dkoski4@yahoo.com>
2021-01-26 12:20:08 -0800

    added sticky header, projects page, contact page, and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomePage />
+      {/* <HomePage /> */}
       <Footer />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,43 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Header(props) {
+  const fixedText = "fixed";
+  const whenNotFixed = "not fixed";
+  const [headerText, setHeaderText] = useState(fixedText);
+  useEffect(() => {
+    const header = document.getElementById("Header");
+    const sticky = header.offsetTop;
+    const scrollCallBack = window.addEventListener("scroll", () => {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        if (headerText !== fixedText) {
+          setHeaderText(fixedText);
+        }
+      } else {
+        header.classList.remove("sticky");
+        if (headerText !== whenNotFixed) {
+          setHeaderText(whenNotFixed);
+        }
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", scrollCallBack);
+    };
+  });
+
   return (
-    <div>
-      <header>
+    <header id="Header">
+      <Link to="/" className="home-link">
         <h1>Danielle Koski</h1>
-        <p>
-          Software Engineer <br />
-          San Francisco Bay Area <br />
-          dkoski426@gmail.com <br />
-          (510) 921-0641
-        </p>
-      </header>
-    </div>
+      </Link>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
   );
 }
 
