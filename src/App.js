b72f9b28468f2c96fd18e71366ad7cdd8d56ce40
Danielle Koski <dkoski4@yahoo.com>
2021-01-25 21:29:12 -0800

    added header and footer components and a main page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+// import Main from "./pages/Main/Main";
 
 function App() {
   return <div className="App"></div>;
---
+++ src/pages/Main.js
@@ -0,0 +1,5 @@
+function Main(props) {
+  return;
+}
+
+export default Main;
