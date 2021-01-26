c08f85656b252abe21206beb4c1b13b662280a42
Danielle Koski <dkoski4@yahoo.com>
2021-01-26 13:31:40 -0800

    successfully sent an email from backend and using contact page on front end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-// import HomePage from "./pages/HomePage/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
+import ContactPage from "./pages/ContactPage/ContactPage";
+// import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* <HomePage /> */}
-      <Footer />
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route exact path="/" render={(props) => <HomePage />} />
+
+          {/* <Route exact path="/projects" render={(props) => <ProjectsPage />} /> */}
+
+          <Route exact path="/contact" render={(props) => <ContactPage />} />
+        </Switch>
+
+        <Footer />
+      </div>
     </div>
   );
 }
